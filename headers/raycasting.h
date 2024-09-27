2b4c5d973ccdd73f13a3c8fd769547c1d0bc05c2
SimArch21 <simon.archambault@hotmail.fr>
2024-09-27 19:34:49 +0200

    commit of the day

--- headers/raycasting.h
+++ headers/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:00:14 by simarcha          #+#    #+#             */
-/*   Updated: 2024/09/25 18:04:41 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/09/27 19:18:31 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 
 # define PROJECTION_PLANE_X 320
 # define PROJECTION_PLANE_Y 200
+
+typedef struct s_grid
+{
+	double	x;
+	double	y;
+}	t_grid;
 
 typedef struct raycasting
 {
@@ -29,6 +35,8 @@ typedef struct raycasting
 	
 	double		ray_from_player_x;//distance from player & the end of the ray
 	double		ray_from_player_y;//distance from player & the end of the ray
+
+	double		angle;//angle of our ray starting from the abscissa axis
 	
 	// double		fov;//angle of player's field of view (=> should be in radian ?)
 	// double		ray_x;//position of the pixel for the first ray of our podouble of view
@@ -41,6 +49,8 @@ typedef struct raycasting
 
 //raycasting_utils.c
 double		rounded_down(double position);
+t_grid		convert_pixel_to_grid(t_grid point);
+
 
 
 #endif
