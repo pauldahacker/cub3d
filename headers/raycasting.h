76bdb2ec46c6915d29c51cd170b942c0c809ad8f
SimArch21 <simon.archambault@hotmail.fr>
2024-09-30 19:38:01 +0200

    commit of the day

--- headers/raycasting.h
+++ headers/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:00:14 by simarcha          #+#    #+#             */
-/*   Updated: 2024/09/28 13:00:33 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/09/30 18:50:03 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@
 
 # define PROJECTION_PLANE_X 320
 # define PROJECTION_PLANE_Y 200
+# define BLOCK_SIZE			64
 
 //How do we code the map?
 //It should be an array of int with the coordinates x and y
-//I named it t_grid because this is the unit that we use
-//grid[x][y] is one cell of our map
-typedef struct s_grid
+//I named it t_block because this is the unit that we use
+//block[x][y] is one cell of our map
+typedef struct s_block
 {
 	double	x;
 	double	y;
-}	t_grid;
+}	t_block;
 
 typedef struct raycasting
 {
@@ -37,10 +38,10 @@ typedef struct raycasting
 	// double		plane_x;//camera plane of the player
 	// double		plane_y;//camera plane of the player
 	
-	double		ray_from_player_x;//distance from player & the end of the ray
-	double		ray_from_player_y;//distance from player & the end of the ray
+//	double		ray_from_player_x;//distance from player & the end of the ray
+//	double		ray_from_player_y;//distance from player & the end of the ray
 
-	double		angle;//angle (in degrees) of our ray starting from the abscissa axis
+	double		angle;//angle (in degrees) of our ray starting from the abscissa axis 0 ≤ angle ≤360
 	
 	// double		fov;//angle of player's field of view (=> should be in radian ?)
 	// double		ray_x;//position of the pixel for the first ray of our podouble of view
@@ -52,7 +53,7 @@ typedef struct raycasting
 
 //raycasting_utils.c
 double		rounded_down(double position);
-t_grid		convert_pixel_to_grid(t_grid point);
+t_block		convert_pixel_to_block(t_block point);
 
 
 
