a4d13badaa80921767c6f30ea855b5a0dce4dde6
SimArch21 <simon.archambault@hotmail.fr>
2024-10-09 20:09:18 +0200

    commit of the day

--- srcs/raycasting/vertical_intersection.c
+++ srcs/raycasting/vertical_intersection.c
@@ -6,69 +6,69 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 18:22:16 by simarcha          #+#    #+#             */
-/*   Updated: 2024/10/09 19:43:20 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/10/09 20:08:54 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/cub3d.h"
 
 //I've coded the following functions thanks to this reference: 
-//https://permadi.com/1996/05/ray-casting-tutorial-7/
+//https://permadi.com/1996/05/player-casting-tutorial-7/
 
-t_block	vertical_coordinate_first_block_point(t_player *ray)
+t_block	vertical_coordinate_first_block_point(t_player *player)
 {
 	t_block	a;
 	
-	printf("ray->pos_x = %f\nray->pos_y = %f\nray->angle = %f\n\n", 
-			ray->pos_x, ray->pos_y, ray->angle);
-	if (ray_facing_right(ray->angle) == 1)
-		a.x = rounded_down(ray->pos_x / BLOCK_SIZE) * BLOCK_SIZE + BLOCK_SIZE;
+	printf("player->pos_x = %f\nplayer->pos_y = %f\nplayer->angle = %f\n\n", 
+			player->pos_x, player->pos_y, player->angle);
+	if (player_facing_right(player->angle) == 1)
+		a.x = rounded_down(player->pos_x / BLOCK_SIZE) * BLOCK_SIZE + BLOCK_SIZE;
 	else
-		a.x = rounded_down(ray->pos_x / BLOCK_SIZE) * BLOCK_SIZE - 1;
+		a.x = rounded_down(player->pos_x / BLOCK_SIZE) * BLOCK_SIZE - 1;
 
-	check_vertical_angle_value(ray);
-	printf("value before multiplication = %f\n", ray->pos_y + (ray->pos_x - a.x));
-	printf("multiplication = %f\n", tan(ray->angle * (PI / 180)));
-	a.y = ray->pos_y + (ray->pos_x - a.x) * tan(ray->angle * (PI / 180));
-	printf("ray->pos_x = %f\nray->pos_y = %f\nray->angle = %f\n\n", 
-			ray->pos_x, ray->pos_y, ray->angle);
+	check_vertical_angle_value(player);
+	printf("value before multiplication = %f\n", player->pos_y + (player->pos_x - a.x));
+	printf("multiplication = %f\n", tan(player->angle * (PI / 180)));
+	a.y = player->pos_y + (player->pos_x - a.x) * tan(player->angle * (PI / 180));
+	printf("player->pos_x = %f\nplayer->pos_y = %f\nplayer->angle = %f\n\n", 
+			player->pos_x, player->pos_y, player->angle);
 
 	printf("a.x = %f\na.y = %f\n", a.x, a.y);
 	return (a);
 }
 
-double	find_vertical_x_a(t_player *ray)
+double	find_vertical_x_a(t_player *player)
 {
-	if (ray_facing_right(ray->angle) == 1)
+	if (player_facing_right(player->angle) == 1)
 		return ((double)BLOCK_SIZE);
 	return ((double)-BLOCK_SIZE);
 }
 
 //formula from the Pythagoras theorem to find the distance of the hypotenuse
 //how to find the hypotenuse ? 
-//We know the angle (ray->ange) and the distance of one side of the triangle
+//We know the angle (player->ange) and the distance of one side of the triangle
 //which is one cube (<=> BLOCK_SIZE unit)
-double	find_vertical_y_a(t_player *ray)
+double	find_vertical_y_a(t_player *player)
 {
-	printf("ray->angle = %f\n", ray->angle);
-	return ((double)BLOCK_SIZE * tan(ray->angle * (PI / 180)));
+	printf("player->angle = %f\n", player->angle);
+	return ((double)BLOCK_SIZE * tan(player->angle * (PI / 180)));
 }
 
-t_block	vertical_coordinate_next_block_point(t_player *ray, t_block previous)
+t_block	vertical_coordinate_next_block_point(t_player *player, t_block previous)
 {
 	t_block	next;
 	double	x_a;
 	double	y_a;
 
-	x_a = find_vertical_x_a(ray);
-	y_a = find_vertical_y_a(ray);
+	x_a = find_vertical_x_a(player);
+	y_a = find_vertical_y_a(player);
 	printf("y_a find_vertical_y_a = %f\n", y_a);
 	next.x = previous.x + x_a;
 	next.y = previous.y + y_a;
 	return (next);
 }
 
-t_block	vertical_point_crossing_wall(char **map, t_player *ray)
+t_block	vertical_point_crossing_wall(t_vars *vars)
 {
 	t_block	current_in_block;
 	t_block	current_in_px;
@@ -76,33 +76,33 @@ t_block	vertical_point_crossing_wall(char **map, t_player *ray)
 	t_block	next_in_px;
 	int		i = 0;
 
-	current_in_px = vertical_coordinate_first_block_point(ray);//in pixel
+	current_in_px = vertical_coordinate_first_block_point(vars->game->player);//in pixel
 	current_in_block = convert_pixel_to_block(current_in_px);//in block/cub unit
 	printf("current point y = %f && x = %f\n", current_in_px.y, current_in_px.x);
 	printf("current point y = %f && x = %f\n", current_in_block.y, current_in_block.x);
 	current_in_block.x = rounded_nearest_nb(current_in_block.x);
 	current_in_block.y = rounded_nearest_nb(current_in_block.y);
-	while (map[(int)current_in_block.y][(int)current_in_block.x] == '0'
-			|| map[(int)current_in_block.y][(int)current_in_block.x] == 'N')
+	while (vars->game->map[(int)current_in_block.y][(int)current_in_block.x] == '0'
+			|| vars->game->map[(int)current_in_block.y][(int)current_in_block.x] == 'N')
 	{
-		printf("i = %i\ncurrent point y = %f && x = %f: _%c_\n", i++, current_in_block.y, current_in_block.x, map[(int)current_in_block.y][(int)current_in_block.x]);
-		next_in_px = vertical_coordinate_next_block_point(ray, current_in_px);
+		printf("i = %i\ncurrent point y = %f && x = %f: _%c_\n", i++, current_in_block.y, current_in_block.x, vars->game->map[(int)current_in_block.y][(int)current_in_block.x]);
+		next_in_px = vertical_coordinate_next_block_point(vars->game->player, current_in_px);
 		next_in_block = convert_pixel_to_block(next_in_px);
 		next_in_block.x = rounded_nearest_nb(next_in_block.x);
 		next_in_block.y = rounded_nearest_nb(next_in_block.y);
-		if (coordinates_in_map(map, next_in_block) == 0)
+		if (coordinates_in_map(vars->game->map, next_in_block) == 0)
 		{
-			printf("final point y = %f && x = %f: _%c_\n", current_in_block.y, current_in_block.x, map[(int)current_in_block.y][(int)current_in_block.x]);
+			printf("final point y = %f && x = %f: _%c_\n", current_in_block.y, current_in_block.x, vars->game->map[(int)current_in_block.y][(int)current_in_block.x]);
 			return (current_in_px);
 		}
 		current_in_px = next_in_px;
 		current_in_block = next_in_block;
 		printf("current point y = %f\n", current_in_block.y);
 		printf("current point x = %f\n", current_in_block.x);
-		printf("map[y][x] = %c\n", map[(int)current_in_block.y][(int)current_in_block.x]);
+		printf("map[y][x] = %c\n", vars->game->map[(int)current_in_block.y][(int)current_in_block.x]);
 	}
 	printf("5\n");
-	printf("final point y = %f && x = %f: _%c_\n", next_in_block.y, next_in_block.x, map[(int)next_in_block.y][(int)next_in_block.x]);
+	printf("final point y = %f && x = %f: _%c_\n", next_in_block.y, next_in_block.x, vars->game->map[(int)next_in_block.y][(int)next_in_block.x]);
 	return (next_in_px);
 }
 
