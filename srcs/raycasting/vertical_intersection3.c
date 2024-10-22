c91ee8f42269dff0ad1640aea4c2a6fc3571525e
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2024-10-22 19:14:34 +0200

    commit of the day

--- srcs/raycasting/vertical_intersection3.c
+++ srcs/raycasting/vertical_intersection3.c
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:02:55 by simarcha          #+#    #+#             */
-/*   Updated: 2024/10/22 16:56:58 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:30:33 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ t_block	find_coordinate_of_first_vertical_point(t_vars *vars, double ray_angle)
 	else
 		a.x = rounded_down(vars->game->player->pos_x / BLOCK_SIZE) * BLOCK_SIZE - 1;
 	a.y = vars->game->player->pos_y + (vars->game->player->pos_x - a.x) * tan(ray_angle * (PI / 180.0));
+	printf("find_coordinate_of_first_vertical_point first_point a.x = %f && a.y = %f\n", a.x, a.y);
 	return (a);
 }
 
@@ -53,6 +54,7 @@ t_block	find_next_vertical_point(t_block current_point, double ray_angle)
 	next_in_px.x = current_point.x + x_a;
 	next_in_px.y = current_point.y + y_a;
 	next_in_px.reachable = true;
+	printf("next_point.x = %f && next_point.y = %f\n", next_in_px.x, next_in_px.y);
 	return (next_in_px);
 }
 
