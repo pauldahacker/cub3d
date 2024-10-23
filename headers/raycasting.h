da92905f8a7f4d11baacfee0fa7a74653b8f5cd7
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2024-10-23 16:32:31 +0200

    commit of the day

--- headers/raycasting.h
+++ headers/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:00:14 by simarcha          #+#    #+#             */
-/*   Updated: 2024/10/22 16:46:37 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:32:06 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,35 +25,35 @@ typedef struct s_vars		t_vars;
 
 //How do we code the map?
 //It should be an array of int with the coordinates x and y
-//I named it t_block because this is the unit that we use
+//I named it t_point because this is the unit that we use
 //block[x][y] is one cell of our map
 typedef struct s_block
 {
 	double	x;
 	double	y;
 	bool	reachable;
-}	t_block;
+}	t_point;
 
 //raycasting_utils.c
 double		rounded_down(double nb);
 double		rounded_nearest_nb(double nb);
-t_block		convert_pixel_to_block(t_block point);
+t_point		convert_pixel_to_block(t_point point);
 int			ray_facing_up(double angle);
 int			ray_facing_right(double angle);
 
 void		check_horizontal_angle_value(t_player *ray);
 void		check_vertical_angle_value(t_player *ray);
-int			check_coordinates_in_map(t_vars *vars, t_block current);
+int			check_coordinates_in_map(t_vars *vars, t_point current);
 
 //horizontal_instersection.c
-t_block		horizontal_point_crossing_wall(t_vars *vars, double ray_angle);
+t_point		horizontal_point_crossing_wall(t_vars *vars, double ray_angle);
 void		init_player_for_test(t_player *ray);
 
 //vertical_instersection.c
-t_block		vertical_point_crossing_wall(t_vars *vars, double ray_angle);
+t_point		vertical_point_crossing_wall(t_vars *vars, double ray_angle);
 
 //double		calculate_best_distance(t_vars *vars, double angle);
-t_block		return_intersection(t_vars *vars, double angle);
+t_point		return_intersection(t_vars *vars, double angle);
 double		calculate_best_distance(t_vars *vars, double angle);
 void		draw_every_ray(t_vars *vars);
 //double		calculate_projected_wall_height(t_vars *vars, double distance_to_wall);
