db56d261aee72dd10666f7358b6974a47e12ad5d
pauldahacker <paul.demasca@gmail.com>
2024-10-25 16:00:50 +0200

    pressing multiple direction/rotation keys at the same time works

--- srcs/controls/controls.c
+++ srcs/controls/controls.c
@@ -23,21 +23,53 @@ int	on_destroy(t_vars *vars)
 
 int	on_keypress(int keysym, t_vars *vars)
 {
-	//(void)vars;
-	printf("Pressed key : %d\n", keysym);
 	if (keysym == ESC)
 		on_destroy(vars);
+	else if (keysym == W)
+		vars->keys.w = 1;
+	else if (keysym == A)
+		vars->keys.a = 1;
+	else if (keysym == S)
+		vars->keys.s = 1;
+	else if (keysym == D)
+		vars->keys.d = 1;
+	else if (keysym == LEFT)
+		vars->keys.left = 1;
+	else if (keysym == RIGHT)
+		vars->keys.right = 1;
+	return (0);
+}
+
+int	on_keyrelease(int keysym, t_vars *vars)
+{
 	if (keysym == W)
+		vars->keys.w = 0;
+	else if (keysym == A)
+		vars->keys.a = 0;
+	else if (keysym == S)
+		vars->keys.s = 0;
+	else if (keysym == D)
+		vars->keys.d = 0;
+	else if (keysym == LEFT)
+		vars->keys.left = 0;
+	else if (keysym == RIGHT)
+		vars->keys.right = 0;
+	return (0);
+}
+
+int	update_player(t_vars *vars)
+{
+	if (vars->keys.w)
 		on_move_up(vars, MOVEMENT_SPEED);
-	if (keysym == A)
+	if (vars->keys.a)
 		on_move_left(vars, MOVEMENT_SPEED);
-	if (keysym == S)
+	if (vars->keys.s)
 		on_move_down(vars, MOVEMENT_SPEED);
-	if (keysym == D)
+	if (vars->keys.d)
 		on_move_right(vars, MOVEMENT_SPEED);
-	if (keysym == LEFT)
+	if (vars->keys.left)
 		on_rotate_left(vars, ROTATE_SPEED);
-	if (keysym == RIGHT)
+	if (vars->keys.right)
 		on_rotate_right(vars, ROTATE_SPEED);
 	return (0);
 }
