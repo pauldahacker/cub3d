adab71b6d3369c041c4e79b65f58e7e8edcd0954
SimArch21 <simon.archambault@hotmail.fr>
2024-10-29 12:41:38 +0100

    commit of the day

--- srcs/raycasting/raycasting_utils.c
+++ srcs/raycasting/raycasting_utils.c
@@ -6,7 +6,7 @@
 /*   By: simon <simon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:47:35 by simarcha          #+#    #+#             */
-/*   Updated: 2024/10/29 12:15:10 by simon            ###   ########.fr       */
+/*   Updated: 2024/10/29 12:34:39 by simon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,8 +79,8 @@ t_block	convert_pixel_to_block(t_block point)
 {
 	t_block	converted;
 
-	converted.x = rounded_down(point.x / (double)BLOCK_SIZE);
-	converted.y = rounded_down(point.y / (double)BLOCK_SIZE);
+	converted.x = point.x / BLOCK_SIZE;
+	converted.y = point.y / BLOCK_SIZE;
 	return (converted);
 }
 
