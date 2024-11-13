30fc633bbb7b9c1d6c5d0ddcb739ebcf1a0e9638
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2024-11-13 18:43:57 +0100

    commit of the day

--- srcs/raycasting/draw_textures.c
+++ srcs/raycasting/draw_textures.c
@@ -6,7 +6,7 @@
 /*   By: simarcha <simarcha@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:15:43 by simarcha          #+#    #+#             */
-/*   Updated: 2024/11/12 11:12:36 by simarcha         ###   ########.fr       */
+/*   Updated: 2024/11/13 18:43:25 by simarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,34 @@
 
 void	draw_texture(t_vars *vars, int x, int *y, t_texture tex)
 {
-	t_player		*player;
 	unsigned int	color;
 	int				pixel_offset;
+	double			top_y;
+	double			low_y;
+	double			tex_x;
 
-	player = vars->game->player;
-	if (player->former_block_touched.reachable == false)//it means that this is the first ray and former_block_touched is not set yet
-		player->former_block_touched = player->block_touched;//we set it
-	pixel_offset = (*y % 64) * tex.size_line + (x % 64) * (tex.bpp / 8);
-	color = (tex.data[pixel_offset]) << 16 | (tex.data[pixel_offset + 1]) << 8
-		|(tex.data[pixel_offset + 2]);
-	if (rounded_down(player->former_block_touched.x) == rounded_down(player->block_touched.x)
-			&& rounded_down(player->former_block_touched.y) == rounded_down(player->block_touched.y))
-		my_mlx_pixel_put(*vars, x, *y, color);
+	top_y = vars->game->player->proj_plan.wall_top_pos_y_in_px;
+	low_y = vars->game->player->proj_plan.wall_lower_pos_y_in_px;
+	if (fmod(round(vars->game->player->point_hit.x), BLOCK_SIZE) <= 0.000001)
+	{
+		tex_x = fmod(vars->game->player->point_hit.y, BLOCK_SIZE)
+			/ BLOCK_SIZE * tex.width;
+	}
 	else
 	{
-		color = WHITE;
-		while (*y <= player->proj_plan.wall_lower_pos_y_in_px + 1)
-			my_mlx_pixel_put(*vars, x, (*y)++, color);
+		tex_x = fmod(vars->game->player->point_hit.x, BLOCK_SIZE)
+			/ BLOCK_SIZE * tex.width;
 	}
-	if (!(rounded_down(player->former_block_touched.x) == rounded_down(player->block_touched.x)))
-	{
-		player->proj_plan.wall_upper_left_x_in_px = player->former_block_touched.x;
-		player->proj_plan.wall_upper_right_x_in_px = player->block_touched.x;
-		player->horizontal_wall_size_in_px = player->proj_plan.wall_upper_right_x_in_px - player->proj_plan.wall_upper_left_x_in_px;
-		// printf("EN XXXXXX: \nplayer->former_block_touched.x = %f\n", player->former_block_touched.x);
-		// printf("player->block_touched.x = %f\n", player->block_touched.x);
-		// printf("player->horizontal_wall_size_in_px = %f\n", player->horizontal_wall_size_in_px);
-	}
-	else if (!(rounded_down(player->former_block_touched.y) == rounded_down(player->block_touched.y)))
-	{
-		player->proj_plan.wall_upper_left_x_in_px = player->former_block_touched.x;
-		player->proj_plan.wall_upper_right_x_in_px = player->block_touched.x;
-		player->horizontal_wall_size_in_px = player->proj_plan.wall_upper_right_x_in_px - player->proj_plan.wall_upper_left_x_in_px;
-		// printf("EN YYYYYY: \nplayer->former_block_touched.x = %f\n", player->former_block_touched.x);
-		// printf("player->block_touched.x = %f\n", player->block_touched.x);
-		// printf("player->horizontal_wall_size_in_px = %f\n", player->horizontal_wall_size_in_px);
-	}
-	player->former_block_touched = player->block_touched;
+	pixel_offset =  (int)(((*y - top_y) / (low_y - top_y)) * tex.width)
+		* tex.size_line + (int)tex_x * (tex.bpp / 8);
+	color = (tex.data[pixel_offset]) << 16 | (tex.data[pixel_offset + 1]) << 8
+		| (tex.data[pixel_offset + 2]);
+	my_mlx_pixel_put(*vars, x, *y, color);
 }
 
 //we are still drawing column by column
 //firstly we need to know the complete column(y) length of our wall
 //then we need to scale it in function of the texture size
-
 void	draw_wall(t_vars *vars, int *x, int *y)
 {
 	if (ray_facing_up(vars->game->player->ray_angle) == 1
